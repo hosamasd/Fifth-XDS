abc669c59b9f501461409c9910315acbb24bcb4a
hosam <hosamelmalt@gmail.com>
2021-08-10 07:16:48 +0200

    Initial Commit

---
+++ Fifth XDS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fifth XDS
+//
+//  Created by hosam on 10/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fifth XDS/Fifth_XDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fifth_XDSApp.swift
+//  Fifth XDS
+//
+//  Created by hosam on 10/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Fifth_XDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
